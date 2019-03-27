a884b931b8cbb7448c5e6285b2875d639d9a0dc7
omjadas <omja.das@gmail.com>
2019-03-27 21:16:38 +1100

    added support for multiple nodes to JSONReader

--- jsonReader.py
+++ jsonReader.py
@@ -2,15 +2,20 @@ import json
 
 
 class JSONReader():
-    def __init__(self, fname):
+    def __init__(self, fname, offset=0, nodes=1):
         self.f = open(fname, "r", encoding="utf-8")
+        self.offset = offset
+        self.nodes = nodes
         next(self.f)
+        for i in range(offset):
+            next(self.f)
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        line = next(self.f)
+        for i in range(self.nodes):
+            line = next(self.f)
         if line[-2] == ",":
             line = line[:-2]
         if line == "]}\n":
