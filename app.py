7513814bfc473118425bc1680d2bd120beed8ff6
omjadas <omja.das@gmail.com>
2019-03-29 10:23:36 +1100

    improved formatting of output

--- app.py
+++ app.py
@@ -28,11 +28,27 @@ def main():
             for hashtag in tweet["doc"]["entities"]["hashtags"]:
                 hashtags[grid][hashtag["text"].lower()] += 1
 
+    grids = dict(grids)
+    hashtags = dict(hashtags)
+
     grid_data = comm.gather(grids, root=0)
     hashtag_data = comm.gather(hashtags, root=0)
     if rank == 0:
-        print(
-            reduce(lambda x, y: x.update(y) or x, (Counter(d) for d in grid_data)))
+        total_tweets = reduce(
+            lambda x, y: x.update(y) or x,
+            (Counter(d) for d in grid_data))
+
+        for grid in total_tweets.most_common():
+            print("{}: {} posts".format(grid[0], grid[1]))
+
+        new_dict = defaultdict(lambda: Counter())
+        for h in hashtag_data:
+            for grid in h:
+                for hashtag in h[grid]:
+                    new_dict[grid][hashtag] += h[grid][hashtag]
+        for grid in total_tweets.most_common():
+            print("{}: {}".format(grid[0], new_dict[grid[0]].most_common(5)))
+        # print("{}".format(reduce(lambda x, y: x.update(y) or x, {h: Counter(hashtags[h]) for h in hashtags})).encode("utf-8"))
 
     file.close()
 
